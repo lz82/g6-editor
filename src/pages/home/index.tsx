cffe8af08bd9a3350db2255f35219c887d9f80d1
lz82 <nickylau82@gmail.com>
2020-09-27 09:02:21 +0800

    :construction: modify style

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -7,37 +7,22 @@ import DragItem from '@/components/drag-item';
 import DropContainer from '@/components/drop-container';
 
 import css from './index.module.less';
+import { GraphData } from '@antv/g6/lib/types';
+import { tuple } from 'antd/lib/_util/type';
 
-const TOOLBAR_WIDTH = 160;
-const HEADER_HEIGHT = 50;
 const NODE_WIDTH = 80;
+const NODE_HEIGHT = 80;
 
 const Home = () => {
   const editorRef = useRef<HTMLDivElement>(null);
   const minimapRef = useRef<HTMLDivElement>(null);
   const editor = useRef<Graph>();
 
-  let data = {
+  let data: GraphData = {
     // 点集
-    nodes: [
-      {
-        id: 'node1', // String，该节点存在则必须，节点的唯一标识
-        x: 100, // Number，可选，节点位置的 x 值
-        y: 200 // Number，可选，节点位置的 y 值
-      },
-      {
-        id: 'node2', // String，该节点存在则必须，节点的唯一标识
-        x: 300, // Number，可选，节点位置的 x 值
-        y: 200 // Number，可选，节点位置的 y 值
-      }
-    ],
+    nodes: [],
     // 边集
-    edges: [
-      {
-        source: 'node1', // String，必须，起始点 id
-        target: 'node2' // String，必须，目标点 id
-      }
-    ]
+    edges: []
   };
 
   useEffect(() => {
@@ -45,38 +30,81 @@ const Home = () => {
       container: minimapRef.current,
       size: [200, 160]
     });
+    const canvasDom = document.querySelector('#editor-wrapper') as HTMLElement;
+    console.log(canvasDom);
     const grid = new Grid();
+
     editor.current = new G6.Graph({
       container: editorRef.current as HTMLDivElement,
-      width: editorRef.current?.scrollWidth || 1000,
-      height: editorRef.current?.scrollHeight || 800,
+      width: canvasDom.scrollWidth || 1000,
+      height: canvasDom.scrollHeight || 800,
       plugins: [minimap, grid],
       modes: {
         default: ['drag-node']
-      },
-      defaultNode: {
-        size: 80,
-        style: {
-          fill: '#fff',
-          stroke: 'blue',
-          lineWidth: 1
-        }
       }
     });
 
     editor.current.data(data);
 
     editor.current.render();
+
+    editor.current.on('node:mouseenter', (evt: any) => {
+      console.log(typeof evt);
+      const { item } = evt;
+      console.log(item, evt);
+      editor.current?.setItemState(item, 'hover', true);
+      editor.current?.updateItem(item, {
+        linkPoints: {
+          top: true,
+          bottom: true,
+          left: true,
+          right: true,
+          size: 10,
+          fill: '#fff',
+          color: '#eee'
+        }
+      });
+    });
+
+    editor.current.on('node:mouseleave', (evt: any) => {
+      console.log(typeof evt);
+      const { item } = evt;
+      console.log(item, evt);
+      editor.current?.setItemState(item, 'hover', false);
+      editor.current?.updateItem(item, {
+        linkPoints: {
+          top: false,
+          bottom: false,
+          left: false,
+          right: false,
+          size: 10
+        }
+      });
+    });
   }, [data]);
 
   const onDragEnd = (item: { name: string }, position: { x: number; y: number }) => {
-    console.log(item, position);
-    if (position.x > TOOLBAR_WIDTH && position.y > HEADER_HEIGHT) {
+    const point = editor.current?.getPointByClient(position.x, position.y);
+    if (point && point.x > 0 && point.y > 0) {
       // 完全进入画布，则生成一个节点
-      data.nodes.push({
-        id: new Date() + '',
-        x: position.x - (160 - NODE_WIDTH / 2),
-        y: position.y - (50 - NODE_WIDTH / 2)
+      const point = editor.current?.getPointByClient(position.x, position.y);
+      console.log('point', point);
+      data.nodes?.push({
+        id: Math.random() * 10000000 + '',
+        x: (point ? point.x : 0) + NODE_WIDTH / 2,
+        y: (point ? point.y : 0) + NODE_HEIGHT / 2,
+        label: item.name,
+        size: 80,
+        type: 'circle',
+        style: {
+          fill: '#fff',
+          stroke: 'blue',
+          lineWidth: 1
+        },
+        anchorPoints: [
+          [0, 1],
+          [0.5, 1]
+        ]
       });
       editor.current && editor.current.data(data);
 
@@ -98,9 +126,13 @@ const Home = () => {
             </DragItem>
           </ul>
         </div>
-        <DropContainer>
-          <div ref={editorRef} className={css['editor-wrapper']}></div>
-        </DropContainer>
+
+        <div id="editor-wrapper" className={css['editor-wrapper']}>
+          <DropContainer>
+            <div ref={editorRef}></div>
+          </DropContainer>
+        </div>
+
         <div className={css['info-wrapper']}>
           <div ref={minimapRef} className="minimap-container"></div>
         </div>
