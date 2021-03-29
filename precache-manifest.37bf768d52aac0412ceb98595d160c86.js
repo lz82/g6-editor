cd2ff6d72f216900882a5618f10e13e190486894
lz82 <nickylau82@gmail.com>
2021-03-29 23:38:36 +0800

    :construction: add qiankun

---
+++ precache-manifest.37bf768d52aac0412ceb98595d160c86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a45f3f1fcd5c32ae50ad25dd8fcdb1d8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ad00a4ebf5172899e184",
+    "url": "./static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "7254dc98bf2b752b9f3c",
+    "url": "./static/css/main.29c84c4b.chunk.css"
+  },
+  {
+    "revision": "ad00a4ebf5172899e184",
+    "url": "./static/js/2.29edaa05.chunk.js"
+  },
+  {
+    "revision": "d4c0231915bd4972d332792a7d92551f",
+    "url": "./static/js/2.29edaa05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7254dc98bf2b752b9f3c",
+    "url": "./static/js/main.ffd9246b.chunk.js"
+  },
+  {
+    "revision": "ede7e8033eec9e77e0a0",
+    "url": "./static/js/runtime-main.6036040b.js"
+  }
+]);
