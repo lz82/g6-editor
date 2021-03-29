96ee77991714cf97973c9902f2893dbe5a992cf0
lz82 <nickylau82@gmail.com>
2021-03-29 23:20:44 +0800

    :construction: use main app router

--- src/index.tsx
+++ src/index.tsx
@@ -25,14 +25,20 @@ if (!window.__POWERED_BY_QIANKUN__) {
  * bootstrap 只会在微应用初始化的时候调用一次，下次微应用重新进入时会直接调用 mount 钩子，不会再重复触发 bootstrap。
  * 通常我们可以在这里做一些全局变量的初始化，比如不会在 unmount 阶段被销毁的应用级别的缓存等。
  */
-export async function bootstrap() {
-  console.log('react app bootstraped');
+export async function bootstrap(props: any) {
+  console.log('react app bootstraped', props);
 }
 /**
  * 应用每次进入都会调用 mount 方法，通常我们在这里触发应用的渲染方法
  */
 export async function mount(props: any) {
-  console.log(props);
+  // console.log(props);
+  const { token, history } = props;
+  // 将token注入cookie、全局状态等
+  console.log(token);
+  setTimeout(() => {
+    history.push('/admin/user/list');
+  }, 3000);
   render();
 }
 /**
